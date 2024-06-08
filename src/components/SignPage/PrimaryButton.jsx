c47dcae5da89411835e9d4a148bcffa26ac76be2
Makutu369 <brightdreamfuture6@gmail.com>
2024-06-08 10:18:12 +0000

    fixed the primary button

--- src/components/SignPage/PrimaryButton.jsx
+++ src/components/SignPage/PrimaryButton.jsx
@@ -1,18 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import SearchUp from "../../assets/search-up-right";
-import { motion } from "framer-motion";
 // eslint-disable-next-line react/prop-types
 const PrimaryButton = ({ input, route }) => {
   const navigate = useNavigate();
   return (
-    <motion.div
-      whileTap={{ scale: 0.9 }}
+    <div
       onClick={() => navigate(`/${route}`, { replace: false })}
-      className="flex bg-primary h-20 text-nowrap text-lg px-6 rounded-full  text-black justify-between items-center w-64"
+      className=" bg-primary/80 active:bg-primary/70 transition-colors cursor-pointer h-20 inline-flex text-lg px-6 rounded-full text-black justify-between items-center w-52 "
     >
-      <span>{input}</span>
-      <SearchUp />
-    </motion.div>
+      {input}
+    </div>
   );
 };
 
