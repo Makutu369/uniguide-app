99ef455178fe034b80be3b125d053214725379fe
Makutu369 <brightdreamfuture6@gmail.com>
2024-06-11 14:03:02 +0000

    fixed the route

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 
 const Verified = () => {
   const location = useLocation();
-  const queryparam = URLSearchParams(location.search);
+  const queryparam = new URLSearchParams(location.search);
   const value = queryparam.get("q");
   return (
     <div className="h-screen flex justify-center items-center w-full bg-slate-950 text-2xl font-boldm font-extralight">
