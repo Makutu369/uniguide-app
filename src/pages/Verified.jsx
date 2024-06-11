38f9ad2a4044c9a67b176c85a222eb06bb38dfd9
Makutu369 <brightdreamfuture6@gmail.com>
2024-06-11 14:27:43 +0000

    working on verify route

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -1,12 +1,24 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { json, useLocation } from "react-router-dom";
 
 const Verified = () => {
   const location = useLocation();
   const queryparam = new URLSearchParams(location.search);
   const value = queryparam.get("q");
+  const [data, setData] = useState();
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://uniguide-back.onrender.com/verify-email?q=${value}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    })();
+  }, [value]);
   return (
     <div className="h-screen flex justify-center items-center w-full bg-slate-950 text-2xl font-boldm font-extralight">
-      <span>Verified {value}</span>
+      <span>Verified {JSON.stringify(data)}</span>
     </div>
   );
 };
