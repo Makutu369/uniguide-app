b36ed9b219f28056ff7251ca765b033a25bf553a
Makutu369 <brightdreamfuture6@gmail.com>
2024-06-11 16:02:41 +0000

    verification

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Verified = () => {
   const location = useLocation();
   const queryparam = new URLSearchParams(location.search);
   const value = queryparam.get("q");
+  const [result, setResult] = useState("");
   useEffect(() => {
     (async () => {
       try {
         const response = await fetch(
           `https://uniguide-back.onrender.com/user/verifyEmail?token=${value}`
         );
+        if (response.status(!200)) {
+          setResult("user not verified");
+          return;
+        }
         const data = await response.json();
         console.log(data);
       } catch (e) {
@@ -20,7 +26,7 @@ const Verified = () => {
   }, [value]);
   return (
     <div className="h-screen flex justify-center items-center w-full bg-slate-950 text-2xl font-boldm font-extralight">
-      <span>Verified </span>
+      <span></span>
     </div>
   );
 };
